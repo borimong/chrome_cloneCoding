81f99b26ae13fa1a2a987456582a7ebca26cf79b
김현수 <gimhyeonsu@gimhyeonsuui-MacBookPro.local>
2022-05-20 02:32:10 +0900

    third commit

--- js/clock.js
+++ js/clock.js
@@ -2,9 +2,11 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
 
-    const date = new Date(); 
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
 }
 
-getClock();
+getClock();//I did a change!
 setInterval(getClock, 1000);
